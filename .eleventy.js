353fbdfed8b512931830a26d414ad1f829b07cb2
guestpixel-byte <146646635+guestpixel-byte@users.noreply.github.com>
2024-08-22 13:33:11 +1000

    cms added

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/admin');
 
     
 
